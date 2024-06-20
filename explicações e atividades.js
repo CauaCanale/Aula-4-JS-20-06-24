b5e45cd20e685d674f5c555c0fd453c809f319eb
CauaCanale <caua.cferreira2@gmail.com>
2024-06-20 18:42:02 -0300

    Add files via upload

---
+++ explicações e atividades.js
@@ -0,0 +1,48 @@
+/*      ATIVIDADES
+ * verificar se o numero é maior que 18
+ *verificar se a palavra é igual a SALVE
+ verificar se o numero é positivo
+ */
+ 
+
+/* Dei um valor ao numero (que é 18) e coloquei o If que significa (SE é), e coloquei novamente a variavel (numero) e coloquei o sinal 
+(>=) que significa MENOR QUE OU IGUAL, e coloquei outro numero para ele verificar se é menor que ou igual ao numero 18, e abri um console.log
+e coloquei a resposta que eu quero que apareça na tela, que é o verdade, e na frente da linha que esta o IF e o resto do codigo eu tenho que 
+abrirfora das aspas o {}.
+*/
+
+ numero = 18
+if (numero >= 14)  {
+
+    console.log('verdade')    
+}
+console.log()
+
+/* dei um nome a uma palavra, que é o SALVE, coloquei o IF que significa (SE É), e abri novamente as aspas, e coloquei o nome palavra dentro
+e osinal de igual (==), e abri parenteses com o nome que eu dei a PALAVRA, que é o (SALVE) e depois das aspas eu abri os colchetes {} 
+e dei alguns espacos na frente dele para que ele fique no final do meu codigo, e abaixo eu ja abri o console.log com a palavra que eu 
+dei o valor desde o começo, que é o (SALVE).
+*/
+
+
+palavra = "Salve"
+if (palavra == "Salve") {
+    console.log ("Salve")
+
+}
+console.log()
+
+
+/* eu começo dando o valor do numero, que é negativo (-1), e depois eu abro o IF,coloco aspas e coloco a variavel numero, com o sinal da
+conta na frente, que é o (>=) que significa MAIOR OU IGUAL E COLOQUEI O NUMERO 0 e fechei as aspas, abri os colchetes e dei alguns espaços
+para o colchete ir para o final do codigo, e depois eu abro o console.log com as aspas e parenteses solo escrito POSITIVO e logo abaixo eu
+escrevo ELSE que significa sempre (negativo ou falso) e depois abro novamente o console.log com a escrita NEGATIVA, ja que eu usei o ELSE 
+que como explicado significa sempre FALSO OU NEGATIVO, e depois acabou, fecho as aspas e esta pronto.
+*/
+
+numero = -1
+if (numero >= 0){
+    console.log('positivo')
+} else{
+    console.log('negativo')
+}
